294e762820b89d8f6c14fc66c3e2f3e0b8683011
CarlaDBlue <146874435+CarlaDBlue@users.noreply.github.com>
2024-02-01 17:20:48 +0000

    Arreglando el Node.h

--- Node.h
+++ Node.h
@@ -1,3 +1,5 @@
+#ifndef NODE_H
+#define NODE_H
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
@@ -12,9 +14,20 @@ class Node {
 		T data;
 		Node<T>* next;
 		Node (T data, Node<T>* next=nullptr)
-		friend ostream& operator<<(ostream &out, cont Node<T> &node)
 		{
-			out<<" "<<node.data;
+			this -> data = data;
+			this -> next = next;
+		}
+		friend ostream& operator<<(ostream &out, const Node<T> &patata)
+		{
+			do 
+			{
+				out<<" "<<patata.data;
+				patata=patata.next;
+			}
+			while (patata.next!=nullptr);
 			return out;
 		}
 };
+
+#endif 
